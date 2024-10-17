2919d70facc378918be22dcddc61c8fe4f8baab1
Cristian Figuerola <crifigshe@gmail.com>
2024-10-17 20:15:17 +0200

    Logs

--- php/src/index.php
+++ php/src/index.php
@@ -4,9 +4,11 @@ require $_SERVER["DOCUMENT_ROOT"] . '/../vendor/autoload.php';
 use CFV\Models\Cotxe;
 use CFV\Models\Motocicleta;
 use CFV\Models\Vehicle;
+use Monolog\Logger;
+use Monolog\Handler\RotatingFileHandler;
 
-
-function mostrarDetallsVehicle(Vehicle $vehicle){
+function mostrarDetallsVehicle(Vehicle $vehicle)
+{
     echo $vehicle->mostrarDetalls() . "\n";
 }
 
@@ -15,3 +17,19 @@ $cotxe = new Cotxe("Audi", "A5");
 
 mostrarDetallsVehicle($moto);
 mostrarDetallsVehicle($cotxe);
+
+$log = new Logger("MiLogger");
+$log->pushHandler(
+    new RotatingFileHandler(
+        $_SERVER["DOCUMENT_ROOT"] . '/../log/milog.log',
+        0,
+        Logger::DEBUG
+    )
+);
+
+$log->debug("Esto es un mensaje de DEBUG");
+$log->info("Esto es un mensaje de INFO");
+$log->warning("Esto es un mensaje de WARNING");
+$log->error("Esto es un mensaje de ERROR");
+$log->critical("Esto es un mensaje de CRITICAL");
+$log->alert("Esto es un mensaje de ALERT");
